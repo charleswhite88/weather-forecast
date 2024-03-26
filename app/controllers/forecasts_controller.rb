87a7521ea6a35b1aa4a96e5897bc1eef61576e1c
dev <dev@me.com>
2024-03-26 12:21:19 -0400

    API: Added Api end points for index and show

---
+++ app/controllers/forecasts_controller.rb
@@ -0,0 +1,22 @@
+class ForecastsController < ApplicationController
+  before_action: permit_weather_params, only: :show
+
+  def index
+    @recently_searched = recently_searched
+  end
+
+  def show
+    @forecasts = ForecastService.new(params).call
+  end
+
+  private
+
+  def permit_weather_params
+    params.permit(:query, :days)
+  end
+
+  def recently_searched
+    forecast_keys = Rails.cache.redis.keys('forecast_*')
+    forecast_keys.select { |key| Rails.cache.read(key).present? }
+  end
+end
